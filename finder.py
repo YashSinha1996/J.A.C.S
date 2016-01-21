cdecd023decb7cdb8241a0474be2b3e42ac9db2c
Yash Pratyush Sinha <yps1112@gmail.com>
2016-01-21 17:08:26 +0530

    Fixed timeout compilation bug

--- finder.py
+++ finder.py
@@ -35,7 +35,7 @@ def file_reg(regex,file_list):
             return True
     return False
 
-def file_norm(file,file_list):
+def file_norm(folder,file_list):
     if not file_list:
         return True
     if folder in file_list:
@@ -59,9 +59,9 @@ def evaluate(corr,inp,folder,fault,limit,time,is_file,file_check=None,all_regex=
 
     for to_test in files:
         if is_file:
-            how_ss=tester.from_file(corr=corr,inp=inp,folder=folder,file=os.path.join(folder, to_test),fault=fault,limit=limit,time=timeout)
+            how_ss=tester.from_file(corr=corr,inp=inp,folder=folder,file_check=os.path.join(folder, to_test),fault=fault,limit=limit,time=time)
         else:
-            how_ss=tester.regex_test(corr=corr,inp=inp,folder=folder,file=os.path.join(folder, to_test),fault=fault,limit=limit,time=timeout)
+            how_ss=tester.regex_test(corr=corr,inp=inp,folder=folder,file_check=os.path.join(folder, to_test),fault=fault,limit=limit,time=time)
         how=how_ss[0]
         if how==0:
             print ("All Tests passed for %s" % to_test)
@@ -75,12 +75,16 @@ def evaluate(corr,inp,folder,fault,limit,time,is_file,file_check=None,all_regex=
             print ("The correct version didn't compile properly. Please check it")
         elif how==5:
             print ("The input didn't compile properly")
+        elif how==6:
+            print("Uncheckable File")
+        else:
+            print("how ",how)
 
 def crawlDir(corr,inp,folder,fault,limit,time,is_file,level,file_check=None,multi_check=[],all_regex=False):
     if level==0:
-        evaluate(corr=corr,inp=inp,folder=folder,fault=fault,limit=limit,time=timeout,is_file=is_file,file_check=file_check,all_regex=all_regex)
+        evaluate(corr=corr,inp=inp,folder=folder,fault=fault,limit=limit,time=time,is_file=is_file,file_check=file_check,all_regex=all_regex)
         return
     checker=all_regex and reg_matcher or norm_matcher
     sub_dir_list=[sub_dir for sub_dir in os.listdir(folder) if os.path.isdir(os.path.join(folder, sub_dir)) and checker(folder,multi_check,level)]
     for sub_dir in sub_dir_list:
-        crawlDir(corr=corr,inp=inp,folder=os.path.join(folder, sub_dir),fault=fault,limit=limit,time=timeout,is_file=is_file,level=level-1,file_check=file_check,multi_check=multi_check,all_regex=all_regex)
+        crawlDir(corr=corr,inp=inp,folder=os.path.join(folder, sub_dir),fault=fault,limit=limit,time=time,is_file=is_file,level=level-1,file_check=file_check,multi_check=multi_check,all_regex=all_regex)
